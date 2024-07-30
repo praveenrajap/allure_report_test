562bc9bf10674999ccf79e67f9f41d0c4ba7ae0d
praveenrajap <praveenrajap@users.noreply.github.com>
2024-07-30 19:56:21 +0000

    deploy: 42e03c8adfe1e17499ce94748cda47aca90ccfcc

--- globals/globals.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    productSummaryPage: {
-        ACCOUNT_SECTIONS: ['Current Accounts', 'Savings Accounts', 'Term Deposit Accounts', 'Loans', 'Credit Card Accounts'],
-        MAIN_HEADING_TITLE: "My products"
-    }
-};
--- playwright.service.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-const { getServiceConfig } = require('@azure/microsoft-playwright-testing-private-preview');
-const config = require('./playwright.config');
-
-/* Learn more about service configuration at https://aka.ms/mpt/config */
-export default defineConfig(config, getServiceConfig(config));
