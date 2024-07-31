c62cb8640434b91c18c7130f4339a1be50bbb6ba
praveenrajap <praveenrajap@microsoft.com>
2024-08-01 02:53:01 +0530

    test commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { testPlanFilter } from "allure-playwright/dist/testplan";
  */
 export default defineConfig({
   // grep: testPlanFilter(),
-  reporter: "allure-playwright",
+ 
   testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
